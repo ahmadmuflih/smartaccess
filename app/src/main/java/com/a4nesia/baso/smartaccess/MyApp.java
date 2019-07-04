f377a90e8e8fc1b653819c16e95acc6fd3b722ef
Baso Ahmad Muflih <baso@4nesia.com>
2019-07-04 16:43:23 +0700

    s

--- app/src/main/java/com/a4nesia/baso/smartaccess/MyApp.java
+++ app/src/main/java/com/a4nesia/baso/smartaccess/MyApp.java
@@ -6,7 +6,7 @@ import android.content.Context;
 
 public class MyApp extends Application {
     private static Context mContext;
-    public static final String BASE_URL = "http://192.168.1.13/api/mobile/";
+    public static final String BASE_URL = "http://192.168.1.31/api/mobile/";
     @Override
     public void onCreate() {
         super.onCreate();
